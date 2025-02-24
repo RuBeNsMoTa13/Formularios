210f2749a99464b83a50ab76bc157e94bb808322
rubens13mota@gmail.com <rubens13mota@gmail.com>
2025-02-24 10:44:11 -0300

    css

--- public/assets/scripts/footer.js
+++ public/assets/scripts/footer.js
@@ -2,13 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const footer = document.createElement('footer');
   footer.innerHTML = `
     <div class="container">
-      <img src="/public/assets/images/LOGO BSN STRATEGY.png" alt="Footer Logo" />
+      <img src="/public/assets/images/LOGO BSN STRATEGY.png" alt="Footer Logo" class="logo-footer" />
       <div class="footer-info">
         <p>&copy; 2025 Sua Empresa. Todos os direitos reservados.</p>
         <p>Email: contato@empresa.com | Tel: (11) 1234-5678</p>
-        <div class="social-links">
-          <a href="#"><img src="/public/assets/images/linkedin-icon.png" alt="LinkedIn" /></a>
-          <a href="#"><img src="/public/assets/images/instagram-icon.png" alt="Instagram" /></a>
+        <br>
+          <a href="#"><img src="/public/assets/images/linkedin-icon.png" alt="LinkedIn" class="social-links"  /></a>
+          <a href="#"><img src="/public/assets/images/instagram-icon.png" alt="Instagram" class="social-links"  /></a>
         </div>
       </div>
     </div>
