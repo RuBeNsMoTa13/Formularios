049f98f33210e11e1a4848ad59a3c0d4708325e9
Rubens <Rubens13mota@gmail.com>
2025-04-03 14:09:53 -0300

    delete: codigo desnecessario

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite';
-import fs from 'fs-extra';
-import path from 'path';
+
 
 export default defineConfig({
   base: './', // Mantém caminhos relativos para funcionar em qualquer ambiente
   build: {
     outDir: 'dist', // Pasta de saída
-  },
-  plugins: [
-    {
-      name: 'copy-src',
-      closeBundle: () => {
-        fs.copySync(path.resolve(__dirname, 'src'), path.resolve(__dirname, 'dist/src'));
-      }
-    }
-  ]
-});
+  
+},})
